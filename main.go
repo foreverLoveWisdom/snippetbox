124096ca2553b65d13029d84095bbc3986f47b97
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-10-03 21:43:05 +0700

    feat: add Server header to home handler and create POST endpoint for snippet creation
    - Add "Server" header to the response in the home handler function.
    - Create a new function snippetCreatePost to handle POST requests for creating a new snippet.
    - Update the routes in the main function to include the new POST endpoint for snippet creation.

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Server", "Go HTTP Server")
+
 	_, err := w.Write([]byte("Hello World"))
 	if err != nil {
 		log.Printf("failed to write response: %v", err)
@@ -18,14 +20,10 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
+		return
 	}
 
-	msg := fmt.Sprintf("Snippet view: %d", id)
-
-	_, writeErr := w.Write([]byte(msg))
-	if writeErr != nil {
-		log.Printf("failed to write response: %v", writeErr)
-	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreateView(w http.ResponseWriter, _ *http.Request) {
@@ -35,11 +33,22 @@ func snippetCreateView(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func snippetCreatePost(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+
+	_, err := w.Write([]byte("Save new snippet..."))
+	if err != nil {
+		log.Printf("failed to write response: %v", err)
+	}
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreateView)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreateView)
+
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
