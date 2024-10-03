1bf887965cad15cc6fb3c4393e4e5f1780b705cf
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-10-03 13:21:41 +0700

    feat: add wildcard segment for routing

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, _ *http.Request) {
@@ -12,10 +14,17 @@ func home(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func snippetView(w http.ResponseWriter, _ *http.Request) {
-	_, err := w.Write([]byte("Display a specific snippet..."))
-	if err != nil {
-		log.Printf("failed to write response: %v", err)
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+	}
+
+	msg := fmt.Sprintf("Snippet view: %d", id)
+
+	_, writeErr := w.Write([]byte(msg))
+	if writeErr != nil {
+		log.Printf("failed to write response: %v", writeErr)
 	}
 }
 
@@ -26,18 +35,11 @@ func snippetCreateView(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func exampleHandler(_ http.ResponseWriter, r *http.Request) {
-	category := r.PathValue("category")
-	itemID := r.PathValue("itemID")
-	log.Printf("category: %s, itemID: %s", category, itemID)
-}
-
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreateView)
-	mux.HandleFunc("/products/{category}/items/{itemID}/", exampleHandler)
 
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
