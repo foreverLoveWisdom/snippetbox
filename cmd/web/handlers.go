de4ff7272c2167246d23d627424a57499665d18a
foreverLoveWisdom <forever.love.wisdom@gmail.com>
2024-12-23 18:30:22 +0700

    feat: implement basic web server and handlers
    - Add home, snippet view, create, and post handlers
    - Set up HTTP server with routing for snippets
    - Handle errors and responses appropriately

---
+++ cmd/web/handlers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Add("Server", "Go")
+
+	_, err := w.Write([]byte("Hello from Snippetbox"))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+func snippetCreate(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Display a form for creating a new snippet..."))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func snippetCreatePost(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+
+	_, err := w.Write([]byte("Save a new snippet..."))
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
